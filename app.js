09beb23f9266d3b812aa1d9d4a2e1360907f7de4
VirAlvarado <viralvarado.994@gmail.com>
2022-03-19 20:53:20 -0300

    home terminado

--- app.js
+++ app.js
@@ -6,18 +6,22 @@ const port = 3030
 app.use(express.static('public'));
 
 app.get('/', (req,res) => {
-    return res.sendFile(path.resolve(__dirname,'views','index.html'))
+    return res.sendFile(path.resolve(__dirname,'views','index.html'));
+});
+
+app.get('/about',(req,res)=>{
+    return res.sendFile(path.resolve(__dirname, 'views','about.html'));
 });
 
 app.get('/contact',(req,res)=>{
-    return res.sendFile(path.resolve(__dirname, 'views','contact.html'))
+    return res.sendFile(path.resolve(__dirname, 'views','contact.html'));
 });
 
 app.get('/music',(req,res)=>{
-    return res.sendFile(path.resolve(__dirname, 'views','music.html'))
+    return res.sendFile(path.resolve(__dirname, 'views','music.html'));
 });
 
 app.get('*',(req,res)=>{
-    return res.sendFile(path.resolve(__dirname,'views','notFound.html'))
+    return res.sendFile(path.resolve(__dirname,'views','notFound.html'));
 })
-app.listen(port,()=> console.log(`Server runing on port http://localhost:${port}`))
+app.listen(port,()=> console.log(`Server runing on port http://localhost:${port}`));
